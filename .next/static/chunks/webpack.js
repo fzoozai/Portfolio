2dc1136f788fb42acf40a810274c54d198c505d5
Fabian Soosaithasan <fabian.soosaithasan@gmail.com>
2020-10-02 01:18:43 +0200

    Fix image import

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "40571ae0f08280228e53";
+/******/ 	var hotCurrentHash = "4a2cd5ba21240cd5d688";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+const styling = {
+  backgroundImage: `url("./marburg.jpg")`,
+  width:"100%",
+  height:"100%"
+}
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={styling}>
       <h1>Hello Marburg.</h1>
       <ul>
         <li>
